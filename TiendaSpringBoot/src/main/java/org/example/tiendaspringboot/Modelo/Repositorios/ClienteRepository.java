39651eaaf2a33cd4a221aa1409f145fa40c331fe
TampaxCompak <dpf109@educa.madrid.org>
2025-02-21 10:32:38 +0100

    Update ClienteRepository.java

--- TiendaSpringBoot/src/main/java/org/example/tiendaspringboot/Modelo/Repositorios/ClienteRepository.java
+++ TiendaSpringBoot/src/main/java/org/example/tiendaspringboot/Modelo/Repositorios/ClienteRepository.java
@@ -2,7 +2,9 @@ package org.example.tiendaspringboot.Modelo.Repositorios;
 
 import org.example.tiendaspringboot.Modelo.DTOs.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 }
